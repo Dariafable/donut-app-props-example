541ac78d8f5cd7e4317601359d3c7953f2933947
Daria Nebylitsa <daranebylitsa@gmail.com>
2022-08-07 20:10:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './components/hero/Hero'
 import NavBar from './components/navbar/NavBar';
+import Food from './components/food/Food'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar />
       <Hero />
+      <Food />
     </>
   );
 }
