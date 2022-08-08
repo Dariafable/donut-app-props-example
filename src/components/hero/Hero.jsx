f9d3a6fc77e8e8c59b7f1d21d7bbd8a526de7595
Daria Nebylitsa <79958338+Dariafable@users.noreply.github.com>
2022-08-08 16:03:13 +0300

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,15 +5,15 @@ const hero = () => {
   return (
     <div className='hero'>
 
-      <div className="container">
-        <div className="content">
-          <div className="text">
+      <div className='container'>
+        <div className='content'>
+          <div className='text'>
             <p>Always</p>
           </div>
           <h1>Served Fresh</h1>
           <h1>Daily</h1>
           <button className='btn'>Delivery</button>
-          <button className="btn">Take-out</button>
+          <button className='btn'>Take-out</button>
         </div>
       </div>
 
