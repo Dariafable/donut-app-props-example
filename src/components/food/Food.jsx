a67004d2267f5db7cb1f71834c77e990b3f21ace
Daria Nebylitsa <79958338+Dariafable@users.noreply.github.com>
2022-08-08 16:05:18 +0300

    Update Food.jsx

--- src/components/food/Food.jsx
+++ src/components/food/Food.jsx
@@ -5,9 +5,9 @@ const Food = (props) => {
   return (
     <div className='food' style={{ background: `url('${props.bgImgCover}') no-repeat center center/cover`}}>
         <div className='grid-container'>
-            <div className="left" style={{ background: `url('${props.bgImgLeft}') no-repeat center center/cover`}} ></div>
-            <div className="top" style={{ background: `url('${props.bgImgTop}') no-repeat center center/cover`}}></div>
-            <div className="bottom" style={{ background: `url('${props.bgImgBottom}') no-repeat center center/cover`}}></div>
+            <div className='left' style={{ background: `url('${props.bgImgLeft}') no-repeat center center/cover`}} ></div>
+            <div className='top' style={{ background: `url('${props.bgImgTop}') no-repeat center center/cover`}}></div>
+            <div className='bottom' style={{ background: `url('${props.bgImgBottom}') no-repeat center center/cover`}}></div>
         </div>
     </div>
   )
